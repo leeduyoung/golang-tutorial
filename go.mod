7e27bace14929ddf10960d4fed74711282f209c9
kaye <kaye.lee@luxrobo.com>
2021-06-28 12:15:22 +0900

    feat: Add recover

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/evalphobia/logrus_sentry v0.8.2
 	github.com/getsentry/raven-go v0.2.0
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/gorilla/mux v1.8.0
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.8.1
 )
---
+++ server/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	m := mux.NewRouter()
+	m.Handle("/", RecoverWrap(http.HandlerFunc(handler))).Methods("GET")
+	m.Handle("/home", RecoverWrap(http.HandlerFunc(home))).Methods("GET")
+
+	http.Handle("/", m)
+	http.Handle("/home", m)
+	log.Println("Listening...")
+
+	http.ListenAndServe(":3001", nil)
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	panic(errors.New("panicing from error"))
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("say hi")
+}
+
+func RecoverWrap(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			r := recover()
+			if r != nil {
+				var err error
+				switch t := r.(type) {
+				case string:
+					err = errors.New(t)
+				case error:
+					err = t
+				default:
+					err = errors.New("Unknown error")
+				}
+				sendMeMail(err)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		}()
+		h.ServeHTTP(w, r)
+	})
+}
+
+func sendMeMail(err error) {
+	// send mail
+	fmt.Println("called sendMeMail")
+}
