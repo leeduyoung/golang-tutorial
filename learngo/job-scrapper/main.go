23ee71be1074e286dc537c91b1967a8e7fe8f765
dylee <leeduyoung2002@gmail.com>
2022-03-01 23:06:51 +0900

    feat: Job scrapper ing...

--- learngo/job-scrapper/main.go
+++ learngo/job-scrapper/main.go
@@ -9,7 +9,15 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
+var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=3"
+
+type extractedJob struct {
+	id          string
+	title       string
+	companyName string
+	location    string
+	summary     string
+}
 
 func main() {
 	totalCount := getTotalPageCount()
@@ -23,6 +31,25 @@ func main() {
 func getPage(page int) {
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
 	fmt.Println(pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	item := doc.Find(".mosaic-provider-jobcards")
+	item.Find("a").Each(func(i int, s *goquery.Selection) {
+		id, _ := s.Attr("data-jk")
+		title, _ := s.Find(".jobTitle>span").Attr("title")
+		companyName := s.Find(".companyName").Text()
+		companyLocation := s.Find(".companyLocation").Text()
+		summary := s.Find(".job-snippet").Text()
+		fmt.Println(id, title)
+		fmt.Println(companyName, companyLocation, summary)
+	})
 }
 
 func getTotalPageCount() int {
