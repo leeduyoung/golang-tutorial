1bf019604bd1bc78224b3dc8dae9eff608e3221f
dylee <leeduyoung2002@gmail.com>
2022-04-19 22:49:19 +0900

    feat: Add algorithm

--- algorithm/008/main.go
+++ algorithm/008/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 /*
 큰 수 만들기 (탐욕법)
 문제 설명
@@ -17,13 +22,50 @@ number	k	return
 "1924"	2	"94"
 "1231234"	3	"3234"
 "4177252841"	4	"775841"
-출처
+
 */
 
 func main() {
-
+	// fmt.Println(solution("1924", 2))       // 94
+	// fmt.Println(solution("1231234", 3))    // 3234
+	fmt.Println(solution("4177252841", 4)) // 775841
 }
 
 func solution(number string, k int) string {
-	return ""
+
+	/*
+		1. number의 길이 = n 이라고 할떄, n - k 길이의 빈배열(arr)을 하나 만든다.
+		2. number를 하나씩 순회하면서
+		3. arr에 값을 채운다.
+		4. 배열이 비여있는 최초에는 첫번째 값을 넣는다.
+		5. 두번째 값이 arr의 첫번째 값보다 크고 && 뒤에 (남은 숫자 - 1)이 arr의 비어있는 수보다 많으면 값을 변경
+		6. 나머지는 모두 다음 위치에 넣기
+	*/
+
+	n := len(number)
+	arr := []string{}
+	for i := 0; i < n-k; i++ {
+		arr = append(arr, "0")
+	}
+
+	arrIndex := 0
+	for i, v := range number {
+		value := fmt.Sprintf("%c", v)
+
+		if i == 0 {
+			arr[arrIndex] = value
+			arrIndex++
+			continue
+		}
+
+		if arr[arrIndex-1] < value && (n-i-1) >= (n-k-arrIndex) {
+			arr[arrIndex-1] = value
+			continue
+		}
+
+		arr[arrIndex] = value
+		arrIndex++
+	}
+
+	return strings.Join(arr, "")
 }
